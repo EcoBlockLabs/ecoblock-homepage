c9863bcd8b9103bc92a68a6e3725e60782143e8f
acacialWana <kietnam882@gmail.com>
2023-06-21 06:09:15 +0700

    query Client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
 import SreenDesktop from '../containers/SreenDesktop';
 
 const Index = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+      },
+    },
+  });
+
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <SreenDesktop />
-    </div>
+    </QueryClientProvider>
   );
 };
 
